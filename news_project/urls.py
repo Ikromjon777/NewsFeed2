66cdffebfa726203b9344c7434a96c419698c5be
Asadbek <xamidovikromjon007@gmail.com>
2023-07-29 23:54:13 +0500

    asadbek

--- news_project/urls.py
+++ news_project/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
 ]
 
 # Bu muhim narsa ekan
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
